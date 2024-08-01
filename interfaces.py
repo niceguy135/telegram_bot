f6b24b556fb45aa6c9fa89c8fe057d74f1f7acf5
Данов Константин <danov@ancud.local>
2024-08-01 22:36:21 +0300

    interfaces.py: допили абстрактные методы БД; добавил типы данных аргументам

--- interfaces.py
+++ interfaces.py
@@ -1,16 +1,22 @@
 from abc import ABC, abstractmethod
 
+from telebot import types as tg_types
+
 
 class AbstractDatabase(ABC):
 
     @abstractmethod
-    def is_user_exist(self):
+    def is_user_exist(self, user_id: int):
         """Проверяет, есть ли записи о пользователе в БД"""
 
     @abstractmethod
-    def init_start_record(self):
+    def init_start_record(self, user_info: tg_types.User):
         """Создает базовую структуру записей для дальнейшей работы с ними"""
 
     @abstractmethod
-    def create_todo_event(self):
+    def create_todo_event(self, event_date: str, event_desc: str):
         """Создать запись о предстоящем событии"""
+
+    @abstractmethod
+    def get_todo_events(self, user_id: int):
+        """Получить все записи пользователя из ежедневника"""
